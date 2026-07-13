ab56c214ff155cd5f75f79822afc6715693c217f
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-07-13 09:53:47 +0800

    Update k_line.js

--- k_line.js
+++ k_line.js
@@ -1,13 +1,12 @@
 /*
 如果有结果就执行以下代码
 */
-var body = $response.body; //令body为响应体
-/*
-中间这里填想要修改的数据
-*/
-//var cc = '{  "hasAuthorization" : false }'
+var body = $response.body; // 令 body 為響應體
 
-body = body.replace(/hasAuthorization\":\w+/g,'hasAuthorization":true');
+// 修改授權相關欄位
+body = body.replace(/"isPerpetual"\s*:\s*false/g, '"isPerpetual": true');
+body = body.replace(/"expiredTime"\s*:\s*\d+/g, '"expiredTime": 2524579200000');
+body = body.replace(/"unactivatedAuthDays"\s*:\s*\d+/g, '"unactivatedAuthDays": 999');
 
 console.log(body);
-$done(body); //结束
+$done({ body });
