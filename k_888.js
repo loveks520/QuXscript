c0b5311c93edc2926d3169e597e5bf72ce70dff6
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-08-19 06:11:50 +0800

    Update k_888.js

--- k_888.js
+++ k_888.js
@@ -9,4 +9,5 @@ body = body.replace(/"owned"\s*:\s*0/g, '"owned": 1');
 body = body.replace(/"ever_purchased"\s*:\s*0/g, '"ever_purchased": 1');
 body = body.replace(/"subscription"\s*:\s*\d+/g, '"subscription": 99');
 body = body.replace(/"require_pay"\s*:\s*1/g, '"require_pay": 0');
+body = body.replace(/"ua80211_cp"\s*:\s*0/g, '"ua80211_cp": 1');
 $done({ body });
